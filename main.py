05b2c05053b3064baaa50801a32a8a1534d1eebe
sinabiber <chocodufu@gmail.com>
2023-03-30 17:26:50 +0900

    커밋2 회색사진

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ src= cv2.imread("pictures/picture01.jpg")
 
 
 #영상 디스플레이
-cv2.imshow('src', src)
 
+
+dst1=cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+cv2.imshow('dst1', dst1)
 
 
 #마무리
 cv2.waitkey(0)
 cv2.destroyAllWindows()
+
+
 
